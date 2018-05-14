ccdb819a4e790622924a889aa8f8f0dff56772a5
Vicki Hamm <vicki.cello@gmail.com>
2018-05-14 22:59:04 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
-# Your code goes here!
+class Dog
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    @bark = bark
+  end
+
+  def bark=(bark)
+    @bark = bark
+  end
+
+  def bark
+    @bark
+  end
+end
+
+
